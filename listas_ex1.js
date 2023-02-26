b6f46def34c1c11ad20c94e63b768e81d2c57b67
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-02-25 21:19:22 -0400

    Add files via upload

---
+++ listas_ex1.js
@@ -0,0 +1,13 @@
+// Lista em JS
+
+const pokemon = [
+    'Bulbassaur', 
+    'Pikachu',
+    'Squirtle',
+    'Charmander'
+]
+
+console.log(pokemon)        // Exemplo que mostra toda a lista
+console.log(pokemon[2])     // Exemplo que mostra a posição escolhida na lista
+
+// A lista é semelhante a do Python
---
+++ objeto_ex1.js
@@ -0,0 +1,14 @@
+// Objeto em JS
+
+const pessoa =
+{
+    id:'123teste',
+    nome:'Dário da Silva Costa',
+    idade: 38
+}
+
+// Carrega dados de uma variavel 
+// Exemplo mostra uma variavel chamada pessoa que contem os variaveis englobadas
+
+console.log(pessoa)
+console.log(pessoa.nome)
