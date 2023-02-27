1ef7f2eb8027df6523aab480cdaaef5156fa8458
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-02-27 12:14:46 -0400

    Update demo_repetição_1.js

--- demo_repetição_1.js
+++ demo_repetição_1.js
@@ -1,11 +1,13 @@
+// For em JS
+
 let controle;
 
 for(controle = 1; controle <= 10; controle++){
     console.log(controle)
 }
 
-// Exemplo de for em JS
 // Imprime números de 1 a 10
+// Demonstração do funcionamento do FOR
 // 1 - controle = 1;  / iniciando a variavel
 // 2 - controle <= 10 / Limita a execução do for até o valor definido
 // 3 - controle++     / soma a variavel até o limite estabelecido anteriormente
