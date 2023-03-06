cd732777d70332baa191a7b6b3c28a08c01c4da5
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-06 12:56:31 -0400

    Create operador_ternario.js

---
+++ operador_ternario.js
@@ -0,0 +1,8 @@
+let idade = 19
+
+idade >= 18 ? console.log('É maior de idade!') : console.log('Não é maior de idade!')
+
+// O exemplo acima se refere ao operador ternário
+// De certa forma, substitui o if e else (Não sei explicar ainda rs)
+// ? indica se a condição é True e qual será a resposta do sistema
+// : se a primeira condição não for atendida, entra no segundo operador que define a proxima resposta
