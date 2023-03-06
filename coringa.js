c6cdc66b134dffd83f7a47eb21a2097e48b54585
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-06 16:26:10 -0400

    Create coringa.js

---
+++ coringa.js
@@ -0,0 +1,32 @@
+// Coringa JS
+console.log('Conringa JS')
+
+// Tipos de comentário **
+// comentário de uma linha
+
+/* isto é um comentário longo
+   de múltiplas linhas.
+ */
+
+// Sinais de Atribuição **
+// = (Atribuição / let num = 2)
+// == (Igualdade / 2 == 2)
+// === (Estritamente igual, sendo do mesmo valor e tipo de variavel / 2 === 2)
+
+// Declaração de Variáveis **
+
+// var nomeDaVariavel =      / Declara uma variável, opcionalmente, inicializando-a com um valor.
+// let nomeDaVariavel =      / Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+// const variavelConstante = / Declara uma constante de escopo de bloco, apenas de leitura.
+
+// Tipos de Dados **
+
+// Boolean = true e false
+// Null = indica valor nulo
+// undefined = indica valor indefinido
+// Number = Indica Números, 42 ou 3.14159
+// String = Palavras, "Nome"
+// Object
+
+// Listas "Array" Literal **
+// var coffees = ["French Roast", "Colombian", "Kona"];
