a1264feb9a4af2e4035c8ca603758130e3f71290
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-06 13:01:11 -0400

    Update 2_exercicio.js

--- 2_exercicio.js
+++ 2_exercicio.js
@@ -1,8 +1,10 @@
+// Função que faz as 4 operações matématicas
+
 function calculo (num_1, num_2){
-    console.log(num_1, "+" ,num_2, " = ", num_1 + num_2)
+    console.log(num_1, "+" ,num_2, " = ", num_1 + num_2) // Aqui irá mostrar os números a serem somados e seu resultador
     console.log(num_1, "-" ,num_2, " = ", num_1 - num_2)
     console.log(num_1, "*" ,num_2, " = ", num_1 * num_2)
     console.log(num_1, "/" ,num_2, " = ", num_1 / num_2)
 }
 
-calculo(10, 5)
+calculo(10, 5)                                            // Aqui declaramos os números que serão usados durante execução
