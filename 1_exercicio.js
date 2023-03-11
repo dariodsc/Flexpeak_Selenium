a29d7aeeaf54847f9f0c9cc1002dc87547ca6dd1
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-11 15:11:58 -0400

    Update 1_exercicio.js

--- 1_exercicio.js
+++ 1_exercicio.js
@@ -1,4 +1,7 @@
 // função em JS
+// - 1 Exemplo
+// Crie uma função que retorna a string "Olá, " concatenada com um argumento text
+// (a ser passada para a função) e com ponto de exclamação "!" no final
 
 function dizer_ola(){
     console.log('Hello, World!')
@@ -6,3 +9,7 @@ function dizer_ola(){
 
 dizer_ola() // Chama a função
 
+//Exemplo 2
+function cumprimentar(nome) {
+  return "Olá, " + nome + "!";
+}
