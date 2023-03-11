49514e5af6d6896a8a11e4388b286984e4d2a7be
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-11 15:00:37 -0400

    Update 5_Exercicio.js

--- 5_Exercicio.js
+++ 5_Exercicio.js
@@ -6,3 +6,7 @@ function CalcularOperacoes(valor1, valor2){
 }
 
 CalcularOperacoes(3, 5);
+
+// Exercício 5
+// Crie uma função que dados dois valores 
+// (passados como parametros), mostre no console a soma, subtração, multiplicação e divisão desses valores
