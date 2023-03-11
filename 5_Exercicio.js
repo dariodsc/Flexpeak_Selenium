cd98c58b20fdc6ad07695aa26304ba9a6bbf8a4f
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-11 14:48:25 -0400

    Add files via upload

---
+++ 5_Exercicio.js
@@ -0,0 +1,8 @@
+function CalcularOperacoes(valor1, valor2){
+    console.log('Soma', valor1 + valor2);
+    console.log('Subtração', valor1 - valor2);
+    console.log('Multiplicação', valor1 * valor2);
+    console.log('Divisão', valor1 / valor2);
+}
+
+CalcularOperacoes(3, 5);
