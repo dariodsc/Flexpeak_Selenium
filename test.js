c334798ff4356d6d2526eec6c8224cfce4d81a05
dariodsc <46139961+dariodsc@users.noreply.github.com>
2023-03-25 21:20:30 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,41 @@
+const { Builder, By } = require("selenium-webdriver");
+const assert = require("chai").assert
+
+describe('Login Tests', async function () {
+
+    let driver;
+
+    beforeEach(async function () {
+        driver = await new Builder().forBrowser("chrome").build();
+        await driver.get("https://www.saucedemo.com/");
+    /*É executado antes de cada caso de teste, criando uma nova instância do Selenium Webdriver*/
+    });
+
+    afterEach(async function () {
+        await driver.quit()
+    /*É executado após cada caso de teste e fecha o navegador*/
+    });
+
+    it('Login realizado com sucesso', async function () {
+        const campoLogin = await driver.findElement(By.id("user-name")).sendKeys('standard_user')
+        const campoPassword = await driver.findElement(By.id("password")).sendKeys('secret_sauce')
+        const botaoLogin = await driver.findElement(By.id("login-button")).click()
+
+        
+        const title = await driver.findElement(By.className('title')).getText()
+        assert.equal('Products', title)
+    });
+
+    it('Login de um usuário com problemas', async function () {
+        const campoLogin = await driver.findElement(By.id("user-name"))
+        const campoPassword = await driver.findElement(By.id("password"))
+        const botaoLogin = await driver.findElement(By.id("login-button"))
+
+        await campoLogin.sendKeys('problem_user')
+        await campoPassword.sendKeys('secret_sauce')
+        await botaoLogin.click()
+
+        const title = await driver.findElement(By.className('title')).getText()
+        assert.equal('Products', title)
+    });
+});
